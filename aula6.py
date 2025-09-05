c135a49334e5d115b5a57e45f8f53cd790c41b46
Allan Xavier <xavierfmx2017@gmail.com>
2025-09-05 13:22:50 -0300

    Exercicio IMC

---
+++ aula6.py
@@ -0,0 +1,7 @@
+nome = "Allan Xavier"
+altura = 1.80
+peso = 90
+imc = peso / (altura * altura)
+
+print(f"{nome} tem {altura} de altura e pesa {peso} quilos.")
+print(f"O IMC de {nome} é de {imc:.2f}.")
