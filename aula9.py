e0fd68ab01543a32a06326207ae7b4abec70cf65
Allan Xavier <xavierfmx2017@gmail.com>
2025-09-09 14:50:02 -0300

    Aula IF e ELIF

---
+++ aula9.py
@@ -0,0 +1,9 @@
+# if / elif / else
+
+# Exemplo 1
+entrada = input("Voce quer entrar ou sair?: ")
+
+if entrada == "entrar":
+    print("Voce entrou")
+elif entrada == "sair":
+    print("Voce saiu")
